a06b33f7dbc9ec53bd458fbfa21071733647fe38
Paulo Schiavon <paulo@lptennis.com>
2014-05-19 17:37:05 -0300

    Cap 03. My first matcher

--- CC-TDD-Files/my_matchers/spec/my_specs_spec.rb
+++ CC-TDD-Files/my_matchers/spec/my_specs_spec.rb
@@ -5,9 +5,11 @@ class SimplestMatcher
 end
 
 describe 'The matcher protocol' do
-  context 'a minimal matcher' do
-    it 'has a #matches?(actual) method' do
-      expect('anything').to SimplestMatcher.new
-    end 
+  it 'has a #matches?(actual) method' do
+    expect('anything').to simple_matcher
   end
+  
+  def simple_matcher
+    SimplestMatcher.new
+  end 
 end
