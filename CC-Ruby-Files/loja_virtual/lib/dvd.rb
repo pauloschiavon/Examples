51e46d578801847ea8dd567ff85b6f544f8cf4ba
Paulo Schiavon <paulo@lptennis.com>
2014-05-22 14:32:27 -0300

    Ruby. Utilizing a gem

--- CC-Ruby-Files/loja_virtual/lib/dvd.rb
+++ CC-Ruby-Files/loja_virtual/lib/dvd.rb
@@ -1,20 +1,18 @@
 # encoding: utf-8
 
+# coding: utf-8
 class DVD < Midia
-  attr_reader :titulo
   
-  #include FormatadorMoeda
-  
-  #formata_moeda :valor_com_desconto, :valor
+    attr_reader :titulo
 
-  def initialize(titulo, valor, categoria)
-    super()
-    @titulo = titulo
-    @valor = valor
-    @categoria = categoria
-  end
-  
-  def to_s
-    %Q{ Título: #{@titulo}, Valor: #{@valor} }
-  end
+    include FormatadorMoeda
+
+    formata_moeda :valor_com_desconto, :valor
+
+    def initialize(titulo, valor, categoria)
+        super()
+        @titulo = titulo
+        @valor = valor
+        @categoria = categoria
+    end
 end
--- CC-Ruby-Files/loja_virtual/run.rb
+++ CC-Ruby-Files/loja_virtual/run.rb
@@ -3,7 +3,12 @@
 require File.expand_path("lib/loja_virtual")
 
 
-r = Revista.find 3
-r = r.destroy
+# r = Revista.find 3
+# r = r.destroy
+
+windows = DVD.new "Windows 7 for Dummies", 98.9, :sistemas_operacionais
+p windows.valor_por_extenso
+p windows.methods
+# => noventa e oito reais e noventa centavos
 
 
